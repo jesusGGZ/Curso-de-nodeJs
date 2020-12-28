c33734d8d417683c9644db5275a8617182635e59
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-28 11:59:31 -0600

    Presentación de errores e implicaciones de seg

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ router.post('/message', function (req, res) {
     console.log(req.body);
     console.log(req.query);
     if (req.query.error == "ok") {
-        response.error(req, res, 'Error simulado', 400);
+        response.error(req, res, 'Error inesperado', 500,
+        'Es solo una simulacion de los errores');
     }else{
         response.success(req, res, 'Creado correctamente', 201);
     }

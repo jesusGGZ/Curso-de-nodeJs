60d77497464b3c12dd5ac9b88e8c71f640f82ec4
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-28 11:02:56 -0600

    Recibir información desde el cliente: Body y Query

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-// import express form 'express';
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-
+// se indica si se utilizara json, xml, query, etc..
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function (req, res) {
@@ -11,13 +13,11 @@ router.get('/message', function (req, res) {
 });
 
 router.post('/message', function (req, res) {
-    res.send('Mensaje añadido');
+    console.log(req.body);
+    console.log(req.query);
+    // se manda información al cliente
+    res.send('Mensaje ' + req.body.text +' añadido correctamente');
 });
-
-/* app.use('/', function (req, res) {
-    res.send('Hola');
-});
- */
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000');

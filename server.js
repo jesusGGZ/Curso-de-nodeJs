e3f29e7393dfea2d1bcd8d879ab8497e2ce2ba79
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-28 11:43:16 -0600

    Respuestas coherentes: success y error

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const response = require('./network/response');
+
 const router = express.Router();
 
 var app = express();
@@ -16,20 +19,18 @@ router.get('/message', function (req, res) {
         "custom-header": "Nuestro valor personalizado"
     })
 
-    res.send('Lista de mensajes');
+    //res.send('Lista de mensajes');
+    response.success(req, res, 'Lista de mensajes');
 });
 
 router.post('/message', function (req, res) {
     console.log(req.body);
     console.log(req.query);
-    // se manda información al cliente
-    res.send('Mensaje ' + req.body.text +' añadido correctamente');
-});
-
-router.delete('/message', function (req, res) {
-    console.log(req.body);
-    console.log(req.query);
-    res.status(201).send([{error:'', body: 'Creado correctamente'}]);
+    if (req.query.error == "ok") {
+        response.error(req, res, 'Error simulado', 400);
+    }else{
+        response.success(req, res, 'Creado correctamente', 201);
+    }
 });
 
 app.listen(3000);

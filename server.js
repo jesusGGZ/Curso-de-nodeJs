b84b71ec0d75b56a2209d0f8bec55d0428bc69b0
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-29 13:44:21 -0600

    Conectar servidor socket.io

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
-
+const socket = require('./socket');
 const db =require('./db');
-db('mongodb+srv://jesus:jesus123@cluster0.hgjmo.mongodb.net/cursoNode?retryWrites=true&w=majority');
-
 // const router = require('./components/message/network');
 const router = require('./network/routers')
 
-var app = express();
+db('mongodb+srv://jesus:jesus123@cluster0.hgjmo.mongodb.net/cursoNode?retryWrites=true&w=majority');
+
 // se indica si se utilizara json, xml, query, etc..
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 // app.use(router);
 
+socket.connect(server);
 router(app);
 
 // acceso a los archivos de public
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicacion esta escuchando en http://localhost:3000');
+server.listen(3000, function () {
+    console.log('La aplicacion esta escuchando en http://localhost:3000');
+});
 

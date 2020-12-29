96164d458c7e7eb263767e1d434eb50f3740e4ab
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-29 15:02:25 -0600

    Se configuraron mensajes de estado por defecto

--- network/response.js
+++ network/response.js
@@ -1,8 +1,25 @@
+const statusMessages = {
+    '200': 'Done',
+    '201': 'Created',
+    '400': 'Invalid format',
+    '500': 'Internal error',
+}
+
 exports.success = function (req, res, message, status) {
-    //
-    res.status(status || 200 ).send({
+    let statusCode = status;
+    let statusMessage = message;
+
+    if (!status) {
+        status = 200;
+    }
+
+    if (!message) {
+        statusMessage = statusMessages[status];
+    }
+
+    res.status(statusCode ).send({
         error: '',
-        body: message
+        body: statusMessage,
     });
 }
 
@@ -10,8 +27,19 @@ exports.error = function (req, res, message, status, details) {
     //
     console.error('[response error]' + details);
 
-    res.status(status || 500 ).send({
-        error: message,
-        body: ''
+    let statusCode = status;
+    let statusMessage = message;
+
+    if (!status) {
+        status = 500;
+    }
+
+    if (!message) {
+        statusMessage = statusMessages[status];
+    }
+
+    res.status(statusCode ).send({
+        error: statusMessage,
+        body: '',
     });
 }

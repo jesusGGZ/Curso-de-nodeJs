e6299f5e83f86238844e25c480f4b4bc225d3bf9
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-29 12:58:09 -0600

    Guardar fichero en el servidor

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,18 +1,25 @@
 // se agrega el store
 const store = require('./store');
 
-function addMessage(chat, user, message) {
+function addMessage(chat, user, message, file) {
     return new Promise((resolve, reject) => {
         if (!chat || !user || !message) {
             console.error('[messageController] No hay chat, usuario o mensaje');
             reject('Los datos son incorrectos');
             return false;
         }
+
+        let fileUrl = '';
+        if (file) {
+            fileUrl = 'http://localhost:3000/app/files/' + File.filename;
+        }
+
         const fullMessage = {
             chat: chat,
             user: user,
             message: message,
             date: new Date(),
+            file: fileUrl,
         };
         // se agrega el mensaje
         store.add(fullMessage);

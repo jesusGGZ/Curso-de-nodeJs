6f83f0bde1758676b7e0826a45fbd2037cd92b6d
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-29 09:51:29 -0600

    MongoDB: Eliminar datos

--- components/message/controller.js
+++ components/message/controller.js
@@ -34,11 +34,28 @@ function updateMessage(id, message) {
         }
         const result = await store.updateText(id, message);
         resolve(result);
-    })
+    });
+}
+
+function deleteMessage(id) {
+    return new Promise((resolve, reject) =>{
+        if (!id) {
+            reject('Id invalido');
+            return false;
+        }
+        store.remove(id)
+            .then(() => {
+                resolve();
+            })
+            .catch(e => {
+                reject(e);
+            });
+    });
 }
 
 module.exports = {
     addMessage,
     getMessages,
     updateMessage,
+    deleteMessage,
 }

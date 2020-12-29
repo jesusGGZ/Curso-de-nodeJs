60b058ed1d9828d210fd1df96f12b8021619d0d2
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-29 12:44:11 -0600

    Se agrego multer

--- components/message/network.js
+++ components/message/network.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+
 const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
